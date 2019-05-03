69b4339c18455f38be2b246a1d664854c96e1d21
Preeti Sharma <v-preeti.sharma@btpn.com>
2019-05-03 15:53:54 +0700

    Replace directions array with direction function left and right

--- src/Direction.java
+++ src/Direction.java
@@ -4,22 +4,65 @@ public enum Direction {
         public Point changeCoordinates(Point point) {
             return point.incrementY();
         }
+
+        @Override
+        public Direction left() {
+            return WEST;
+        }
+
+        @Override
+        public Direction right() {
+            return EAST;
+        }
     }, EAST {
         @Override
         public Point changeCoordinates(Point point) {
             return point.incrementX();
+        }
+
+        @Override
+        public Direction left() {
+            return NORTH;
+        }
+
+        @Override
+        public Direction right() {
+            return SOUTH;
         }
     }, WEST {
         @Override
         public Point changeCoordinates(Point point) {
             return point.decrementX();
         }
+
+        @Override
+        public Direction left() {
+            return SOUTH;
+        }
+
+        @Override
+        public Direction right() {
+            return NORTH;
+        }
     }, SOUTH {
         @Override
         public Point changeCoordinates(Point point) {
             return point.decrementY();
         }
+
+        @Override
+        public Direction left() {
+            return EAST;
+        }
+
+        @Override
+        public Direction right() {
+            return WEST;
+        }
     };
 
     public abstract Point changeCoordinates(Point point);
+
+    public abstract Direction left();
+    public abstract Direction right();
 }
--- src/Position.java
+++ src/Position.java
@@ -1,15 +1,22 @@
-import java.util.Arrays;
-import java.util.List;
-
 public class Position {
     private final Point point;
     private final Direction direction;
-    private List<Direction> directions;
 
     public Position(Point point, Direction direction) {
         this.point = point;
         this.direction = direction;
-        this.directions = Arrays.asList(Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST);
+    }
+
+    public Position turnLeft() {
+       return new Position(this.point, this.direction.left());
+    }
+
+    public Position turnRight() {
+        return new Position(this.point, this.direction.right());
+    }
+
+    public Position move() {
+        return new Position(direction.changeCoordinates(point), direction);
     }
 
     @Override
@@ -28,25 +35,5 @@ public class Position {
         int result = point != null ? point.hashCode() : 0;
         result = 31 * result + (direction != null ? direction.hashCode() : 0);
         return result;
-    }
-
-    public Position turnLeft() {
-        try{
-            return new Position(this.point, directions.get(directions.indexOf(this.direction) - 1));
-        }catch (ArrayIndexOutOfBoundsException e){
-            return new Position(this.point, directions.get(directions.size()-1));
-        }
-    }
-
-    public Position turnRight() {
-        try{
-            return new Position(this.point, directions.get(directions.indexOf(direction)+1));
-        }catch (ArrayIndexOutOfBoundsException e){
-            return new Position(this.point, directions.get(0));
-        }
-    }
-
-    public Position move() {
-        return new Position(direction.changeCoordinates(point), direction);
     }
 }
